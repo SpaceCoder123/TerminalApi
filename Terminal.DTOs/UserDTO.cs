04756eefae07714026b537232efec012c61e966c
mohanram <mohanraam2000@gmail.com>
2023-08-01 08:46:53 +0530

    changes

--- Terminal.DTOs/UserDTO.cs
+++ Terminal.DTOs/UserDTO.cs
@@ -12,6 +12,9 @@ namespace Terminal.DTOs
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
+        [Required, MinLength(8)]
+        public string Username { get; set; } = string.Empty;
+
         [Required, MinLength(6)]
         public string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
